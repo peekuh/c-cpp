d7eafe9ac9955bf13709d05daed249477045c405
peek <75615590+peekuh@users.noreply.github.com>
2022-09-04 22:38:58 +0530

    n/a

--- pointers/swap.c
+++ pointers/swap.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
+void swap(int *pa, int *pb);
+
 int main()
 {
     int a, b;
     a = 1;
     b = 2;
-
+    swap(&a, &b);
+    printf ("%d %d", a, b);
 }
 
-swap(int x, int y)
-{
-    
+void swap(int *pa, int *pb)
+{   
+    int temp;
+    temp = *pa;
+    *pa = *pb;
+    *pb = temp;
 }
---
+++ pointers/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() 
+{
+    vector<int> v = {1, 2, 3, 4, 5, 6};
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << "\n" ;
+    }
+}
+
+
